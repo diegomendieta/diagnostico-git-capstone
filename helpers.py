1d768152f03e5cd0980956f701991a2cb439f8c9
diegomendieta <damendieta@uc.cl>
2022-03-24 18:18:57 -0300

    Fixed function name

--- helpers.py
+++ helpers.py
@@ -25,7 +25,7 @@ def get_most_retweeted(df, n):
         print('-------------------\n')
 
         
-def get_users_who_tweet_the_most(df, n):
+def get_users_who_tweeted_the_most(df, n):
     top_n_users = df['username'].value_counts()[:n]
     for username, count in zip(top_n_users.index, top_n_users):
         print(f"{(username + ':').ljust(30)}{count}")
